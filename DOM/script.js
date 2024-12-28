df4da25c13498ca244dfbb9bf12a17817bf2e790
hayley <hayley8p@gmail.com>
2024-12-28 23:46:22 +1100

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,29 @@
+// your JavaScript file
+const container = document.querySelector("#container");
+
+const pRed = document.createElement("p");
+pRed.classList.add("pRed");
+pRed.textContent = "Hey, this is red!";
+pRed.style.color = "red";
+
+const h3Blue = document.createElement("h3");
+h3Blue.classList.add("h3Blue");
+h3Blue.textContent = "I’m a blue h3!";
+h3Blue.style.color = "blue";
+
+const newDiv = document.createElement("div");
+newDiv.setAttribute("style", "border: black solid; background-color: pink;");
+
+const newH = document.createElement("h1");
+newH.textContent = "I'm in div"
+const newP = document.createElement("p");
+newP.textContent = "another one";
+
+
+container.appendChild(pRed);
+container.appendChild(h3Blue);
+container.appendChild(newDiv);
+newDiv.appendChild(newH);
+newDiv.appendChild(newP);
+
+
